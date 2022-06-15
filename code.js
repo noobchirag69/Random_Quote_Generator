d8548348762de4b6408d67c0ba758993dc90a1f8
noobchirag69 <chiragchakraborty48@gmail.com>
2022-06-15 12:01:59 +0530

    Comments

--- code.js
+++ code.js
@@ -1,12 +1,14 @@
+// Storing the API endpoint in a variable
 const url = "https://type.fit/api/quotes/";
 
+// Fetching the API
 fetch(url)
     .then(function (response) {
+        // Converting the response into JSON
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
-
+        // Functionality to generate quotes
         let Button = document.querySelector("button");
         Button.addEventListener("click", function () {
             let RandomQuotes = Math.floor(Math.random() * data.length);
